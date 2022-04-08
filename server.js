ce402f23a7871439a042303e3d01f15b97032793
jared <jaredserfozo@gmail.com>
2022-04-08 02:06:47 -0400

    took out bad code

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const favicon = require('serve-favicon')
 const app = express()
 const PORT = 80
 
-npm install
-npm install onoff
-
 
 app.use(favicon(join(__dirname, 'public', 'favicon.ico')))
 app.set('views', join(__dirname, 'views'))
